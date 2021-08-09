45b7388b033ddc404cb7c17a36230fdb63357666
Dmitry Guzeev <dmitri.guzeev@gmail.com>
2021-08-09 12:48:27 +0300

    Fix an issue with Docker Vite file change detection not working on Windows WSL

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,13 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { port: 5000, host: "0.0.0.0" },
+  server: {
+      port: 5000,
+      host: "0.0.0.0",
+      watch: {
+        usePolling: true,
+      },
+  },
   plugins: [reactRefresh()],
   publicDir: "public",
 });
