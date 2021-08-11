dfc5f366bd6863c1a9d1e0fccae022401aec1b7c
Dmitry Guzeev <dmitri.guzeev@gmail.com>
2021-08-11 17:46:27 +0300

    Add typings to server, use dataclasses, change frontend port in docker, fix some bugs, implement word lookup

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,7 +4,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-      port: 5000,
+      port: 8000,
       host: "0.0.0.0",
       watch: {
         usePolling: true,
--- server/server.py
+++ server/server.py
@@ -2,263 +2,386 @@ import logging
 import json
 import aiohttp
 import asyncio
+import random
+from enum import Enum
 from aiohttp import web
 from aiohttp.http_websocket import WSCloseCode, WSMessage
-from itertools import cycle
+from dataclasses import dataclass, field, asdict
+from typing import Dict, Set, List
 
-logger = logging.getLogger(__name__)
-routes = web.RouteTableDef()
+# Environment
+from aiohttp.web_ws import WebSocketResponse
 
+DEV = True
+
+# Logging
+logger = logging.getLogger("server")
+
+
+def configure_loggers():
+    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    if DEV:
+        logger.setLevel(logging.DEBUG)
+        f_handler = logging.FileHandler('server.dev.log')
+        f_handler.setLevel(logging.DEBUG)
+        f_handler.setFormatter(f_format)
+        logger.addHandler(f_handler)
+    else:
+        logger.setLevel(logging.INFO)
+        f_handler = logging.FileHandler('server.prod.log')
+        f_handler.setLevel(logging.INFO)
+        f_handler.setFormatter(f_format)
+        logger.addHandler(f_handler)
+
+
+# Settings
 TIME_UNTIL_START = 4
 LIVES_INITIAL = 3
 MIN_PLAYERS_TO_START_GAME = 2
+TIME_TO_ANSWER = 1
+WORDS_DICT_PATH = "./russian-words/words.txt"
+PARTICLES_PATH = "./russian-words/particles.json"
+
+
+class Difficulty(Enum):
+    MIN_500 = 500
+    MIN_300 = 300
+    MIN_100 = 100
+
+
+@dataclass
+class WordGameDict:
+    words: Set[str] = field(default_factory=lambda: set())
+    particles: Dict[str, int] = field(default_factory=lambda: {})
+    particle_keys: List[str] = field(default_factory=lambda: [])
+    by_difficulty: Dict[Difficulty, List[str]] = field(default_factory=lambda: {})
+
+
+def load_dictionary(words_dict_path: str, particles_path: str) -> WordGameDict:
+    ww = WordGameDict()
+    logger.info("Loading dictionary...")
+    with open(words_dict_path, "r", encoding="windows-1251") as f:
+        for word in f.readlines():
+            ww.words.add(word[:-2])
+    with open(particles_path, "r", encoding="utf-8") as f:
+        ww.particles = json.load(f)
+    ww.particle_keys = list(ww.particles.keys())
+    for diff in Difficulty:
+        particles = list(filter(lambda p: ww.particles[p] >= diff.value, ww.particle_keys))
+        ww.by_difficulty[diff] = particles
+        logger.info("Loaded {} difficulty={} words.".format(len(particles), diff))
+    logger.info("Done. Loaded {} words and {} particles".format(len(ww.words), len(ww.particles)))
+    return ww
+
 
 class CWMSG:
-  Joining = 0
+    """ Client messages. """
+    Joining = 0
+
 
 class SWMSG:
-  InitGame = 0
-  UpdateGameState = 1
+    """ Server messages. """
+    InitGame = 0
+    UpdateGameState = 1
+
 
 class GameStateDesc:
-  WaitingForPlayers = 0
-  Playing = 1
-  Starting = 2
-  Ended = 3
+    """ Description of the current game state. """
+    WaitingForPlayers = 0
+    Playing = 1
+    Starting = 2
+    Ended = 3
 
-def initial_game_state():
-  return {
-    "players": {},
-    "whos_turn": -1,
-    "particle": None,
-    "desc": GameStateDesc.WaitingForPlayers,
-    "start_timer": -1,
-    "user_input": "", # Current user input preview
-    "guess": "",      # User guess (after pressing enter)
-    "last_player_to_answer": None,
-  }
+Message = Dict
+PlayerId = int
 
-words_server_state = {
-  "game_state": initial_game_state(),
-  "clients": {},
-}
+@dataclass
+class PlayerInfo:
+    """ Information on the current player state. """
+    lives_left: int = LIVES_INITIAL
+    nickname: str = None
 
-async def ws_send_to_all(W, msg):
-  print("ws_send_To_all")
-  for _, sock in W["clients"].items():
-    await sock.send_json(msg)
 
-def current_user_id(si):
-  return si["id"]
+@dataclass
+class GameState:
+    """ Current game state. Shared between client and server. """
+    players: Dict[PlayerId, PlayerInfo] = field(default_factory=lambda: {})
+    whos_turn: int = -1
+    particle: str = None
+    desc: GameStateDesc = GameStateDesc.WaitingForPlayers
+    start_timer: int = -1
+    user_input: str = ""  # Current user input preview
+    guess: str = ""  # User guess (after pressing enter)
+    last_player_to_answer: PlayerId = None
 
-async def ws_send_to_others(W, si, msg):
-  print("ws_send_to_others")
-  print(W["clients"], si)
-  cuid = current_user_id(si)
-  for _id, sock in W["clients"].items():
-    if _id != cuid:
-      print(_id)
-      await sock.send_json(msg)
 
-def ws_game_state(W):
-  return W["game_state"]
+def initial_game_state() -> GameState:
+    return GameState()
 
-def state_update_msg(W, *keys):
-  gs = W["game_state"]
-  msg = {
-    "type": SWMSG.UpdateGameState,
-    "state": { key: gs[key] for key in keys }
-  }
-  return msg
 
-async def notify_of_su(W, *keys):
-  msg = state_update_msg(W, *keys)
-  return await ws_send_to_all(W, msg)
+@dataclass
+class ClientInfo:
+    id: PlayerId
+    socket: WebSocketResponse
 
-async def notify_others_of_su(W, si, *keys):
-  msg = state_update_msg(W, *keys)
-  return await ws_send_to_others(W, si, msg)
 
-TIME_TO_ANSWER = 1
+@dataclass
+class ServerState:
+    game_state: GameState = initial_game_state()
+    clients: Dict[PlayerId, ClientInfo] = field(default_factory=lambda: {})
+    ww: WordGameDict = WordGameDict()
 
-async def wrong_guess(W, player):
-  if player["lives_left"] > 0:
-    player["lives_left"] -= 1
-  await notify_of_su(W, "players")
+
+async def ws_send_to_all(W: ServerState, msg: Message):
+    for _, sock in W.clients.items():
+        await sock.send_json(msg)
+
+
+def current_user_id(si: ClientInfo):
+    return si.id
+
+
+async def ws_send_to_others(W: ServerState, si: ClientInfo, msg: Message):
+    cuid = current_user_id(si)
+    for _id, sock in W.clients.items():
+        if _id != cuid:
+            print(_id)
+            await sock.send_json(msg)
+
+
+def state_update_msg(W: ServerState, *keys):
+    gs = W.game_state
+    msg = {
+        "type": SWMSG.UpdateGameState,
+        "state": {key: gs.__dict__[key] for key in keys}
+    }
+    return msg
+
+
+async def notify_of_su(W: ServerState, *keys):
+    msg = state_update_msg(W, *keys)
+    return await ws_send_to_all(W, msg)
+
+
+async def notify_others_of_su(W: ServerState, si: ClientInfo, *keys):
+    msg = state_update_msg(W, *keys)
+    return await ws_send_to_others(W, si, msg)
+
+
+words_server_state = ServerState()
+
+
+async def wrong_guess(W: ServerState, player: PlayerInfo):
+    if player.lives_left > 0:
+        player.lives_left -= 1
+    await notify_of_su(W, "players")
+
+
+async def correct_guess(W: ServerState, player: PlayerInfo):
+    gs = W.game_state
+    gs.guess = ""
+    gs.user_input = ""
+    await notify_of_su(W, "guess", "user_input")
+
 
 def cycle_alive_players(players):
-  while True:
-    player_dead_counted = 0
-    for p in players:
-      player = players[p]
-      if player["lives_left"] == 0:
-        player_dead_counted += 1
-        if player_dead_counted >= len(players):
-          # Everyone is dead so stop the iteration
-          return
-        continue
-      else:
-        yield p, player
-        player_dead_counted = 0
-
-async def end_game(W):
-  gs = W["game_state"]
-  gs["desc"] = GameStateDesc.Ended
-  await notify_of_su(W, "desc", "last_player_to_answer")
-
-async def start_game(W):
-  gs = W["game_state"]
-  gs["desc"] = GameStateDesc.Starting
-  gs["start_timer"] = TIME_UNTIL_START
-  await notify_of_su(W, "desc", "start_timer")
-  for i in range(TIME_UNTIL_START, 0, -1):
-    gs["start_timer"] -= 1
-    await notify_of_su(W, "start_timer")
-    await asyncio.sleep(1)
-  gs["desc"] = GameStateDesc.Playing
-  pit = cycle_alive_players(gs["players"])
-  gs["last_player_to_answer"] = None
-  try:
     while True:
-      # Process current turn
-      player_id, player = next(pit)
-      gs["last_player_to_answer"] = player_id
-      print("Processing player {}".format(player["nickname"]))
-      correct_guess = "tre"
-      print("Guessing {}".format(correct_guess))
-      time_left = TIME_TO_ANSWER
-      gs["whos_turn"] = player_id
-      gs["particle"] = correct_guess
-      gs["user_input"] = ""
-      await notify_of_su(W, "whos_turn", "particle", "user_input", "last_player_to_answer")
-      while True:
-        if gs["guess"] == correct_guess:
-          logger.debug("Correct guess!!")
-          break
-        if time_left <= 0:
-          logger.debug("Failed")
-          await wrong_guess(W, player)
-          break
+        player_dead_counted = 0
+        for p in players:
+            player = players[p]
+            if player.lives_left == 0:
+                player_dead_counted += 1
+                if player_dead_counted >= len(players):
+                    # Everyone is dead so stop the iteration
+                    return
+                continue
+            else:
+                yield p, player
+                player_dead_counted = 0
+
+
+async def end_game(W: ServerState):
+    gs = W.game_state
+    gs.desc = GameStateDesc.Ended
+    await notify_of_su(W, "desc", "last_player_to_answer")
+
+
+def particles_dict_for(W: ServerState, diff: Difficulty):
+    logger.debug("hello i am here")
+    logger.debug(W.ww.by_difficulty)
+    return W.ww.by_difficulty[diff]
+
+
+def is_guess_correct(W: ServerState, guess: str) -> bool:
+    logger.debug(W.ww.words)
+    return guess in W.ww.words
+
+
+async def start_game(W: ServerState):
+    gs = W.game_state
+    gs.desc = GameStateDesc.Starting
+    gs.start_timer = TIME_UNTIL_START
+    pdict = particles_dict_for(W, Difficulty.MIN_500)
+    await notify_of_su(W, "desc", "start_timer")
+    for i in range(TIME_UNTIL_START, 0, -1):
+        gs.start_timer -= 1
+        await notify_of_su(W, "start_timer")
         await asyncio.sleep(1)
-        time_left -= 1
-  except StopIteration:
-    # Game ended
-    await end_game(W)
+    gs.desc = GameStateDesc.Playing
+    pit = cycle_alive_players(gs.players)
+    gs.last_player_to_answer = None
+    try:
+        while True:
+            # Process current turn
+            player_id, player = next(pit)
+            gs.last_player_to_answer = player_id
+            logger.debug("Processing player {}".format(player.nickname))
+            particle = random.choice(pdict)
+            logger.debug("Guessing {}".format(correct_guess))
+            time_left = TIME_TO_ANSWER
+            gs.whos_turn = player_id
+            gs.particle = particle
+            gs.user_input = ""
+            await notify_of_su(W, "whos_turn", "particle", "user_input", "last_player_to_answer")
+            while True:
+                is_correct = is_guess_correct(W, gs.guess)
+                if is_correct:
+                    logger.debug("Correct guess!!")
+                    await correct_guess(W, player)
+                    break
+                if time_left <= 0:
+                    logger.debug("Failed")
+                    await wrong_guess(W, player)
+                    break
+                await asyncio.sleep(1)
+                time_left -= 1
+    except StopIteration:
+        # Game ended
+        await end_game(W)
 
-async def reset_game(W):
-  gs = W["game_state"]
-  gs["desc"] = GameStateDesc.WaitingForPlayers
-  gs["start_timer"] = -1
-  gs["user_input"] = ""
-  gs["particle"] = None
-  gs["whos_turn"] = -1
-  await notify_of_su(W, "desc", "start_timer", "user_input", "particle", "whos_turn")
 
-async def send_to_user(si, msg):
-  return await si["socket"].send_json(msg)
+async def reset_game(W: ServerState):
+    gs = W.game_state
+    gs.desc = GameStateDesc.WaitingForPlayers
+    gs.start_timer = -1
+    gs.user_input = ""
+    gs.particle = None
+    gs.whos_turn = -1
+    await notify_of_su(W, "desc", "start_timer", "user_input", "particle", "whos_turn")
 
-def waiting_for_players(gs):
-  return gs["desc"] == GameStateDesc.WaitingForPlayers
 
-async def on_player_joined(W, gs, si, parsed):
-  nickname = parsed["nickname"]
-  print("{} is joining".format(nickname))
-  # We ahve enough players to start the game
-  ws_id = si["id"]
-  gs["players"][ws_id] = {
-    "nickname": nickname,
-    "lives_left": LIVES_INITIAL,
-    "id": ws_id,
-  }
-  await send_to_user(si, { "type": SWMSG.InitGame, "state": gs })
-  await notify_others_of_su(W, si, "players")
-  if waiting_for_players(gs) and len(gs["players"]) >= MIN_PLAYERS_TO_START_GAME:
-    await start_game(W)
+async def send_to_user(si: ClientInfo, msg: Message):
+    return await si.socket.send_json(msg)
+
+
+def waiting_for_players(gs: GameState):
+    return gs.desc == GameStateDesc.WaitingForPlayers
+
+
+async def on_player_joined(W: ServerState, gs: GameState, si: ClientInfo, parsed: Message):
+    nickname = parsed["nickname"]
+    logger.debug("{} is joining".format(nickname))
+    # We ahve enough players to start the game
+    ws_id = si.id
+    gs.players[ws_id] = PlayerInfo(
+        nickname=nickname,
+        lives_left=LIVES_INITIAL,
+        id=ws_id,
+    )
+    await send_to_user(si, {"type": SWMSG.InitGame, "state": asdict(gs)})
+    await notify_others_of_su(W, si, "players")
+    if waiting_for_players(gs) and len(gs.players) >= MIN_PLAYERS_TO_START_GAME:
+        await start_game(W)
+
 
 ws_handlers_mapping = {
-  CWMSG.Joining: on_player_joined,
+    CWMSG.Joining: on_player_joined,
 }
 
+# Player id
 __player_id = 0
 
+
 def next_player_id():
-  global __player_id
-  __player_id += 1
-  return __player_id
+    global __player_id
+    __player_id += 1
+    return __player_id
 
-async def remove_player(W, si):
-  pid = si["id"]
-  gs = W["game_state"]
-  if W["clients"].get(pid, None) is not None:
-    del W["clients"][pid]
-  if pid in gs["players"]:
-    del gs["players"][pid]
-  if len(gs["players"]) < MIN_PLAYERS_TO_START_GAME:
-    await reset_game(W)
-  await notify_others_of_su(W, si, "players")
 
-def is_user_connected(W, pid):
-  return W["clients"].get(pid, None) is not None
+async def remove_player(W: ServerState, si: ClientInfo):
+    pid = si.id
+    gs = W.game_state
+    if W.clients.get(pid, None) is not None:
+        del W.clients[pid]
+    if pid in gs.players:
+        del gs.players[pid]
+    if len(gs.players) < MIN_PLAYERS_TO_START_GAME:
+        await reset_game(W)
+    await notify_others_of_su(W, si, "players")
+
+
+def is_user_connected(W: ServerState, pid: PlayerId):
+    return W.clients.get(pid, None) is not None
+
+
+###############################
+# Routes
+###############################
+
+routes = web.RouteTableDef()
+
 
 @routes.get('/words/')
 async def words_game(request):
-  ws = web.WebSocketResponse()
-  ready = ws.can_prepare(request=request)
-  if not ready:
-    await ws.close(code=WSCloseCode.PROTOCOL_ERROR)
-  await ws.prepare(request)
-  W = words_server_state
-  gs = ws_game_state(W)
-  pid = next_player_id()
-  logger.info("preparing connection for id={}".format(pid));
-  # TODO: Maybe also check username?
-  if is_user_connected(W, pid):
-    logger.warning("User id={} already connected, disconnecting.".format(pid));
-    await ws.close(code=WSCloseCode.TRY_AGAIN_LATER, message=b'Already connected')
+    """ This route handles the words WebSocket API. """
+    ws = web.WebSocketResponse()
+    ready = ws.can_prepare(request=request)
+    if not ready:
+        await ws.close(code=WSCloseCode.PROTOCOL_ERROR)
+    await ws.prepare(request)
+    W = words_server_state
+    gs = W.game_state
+    pid = next_player_id()
+    logger.info("preparing connection for id={}".format(pid))
+    # TODO: Maybe also check username?
+    if is_user_connected(W, pid):
+        logger.warning("User id={} already connected, disconnecting.".format(pid))
+        await ws.close(code=WSCloseCode.TRY_AGAIN_LATER, message=b'Already connected')
+        return ws
+    # Add user to the client list
+    si = ClientInfo(id=pid, socket=ws)
+    W.clients[pid] = ws
+    try:
+        async for msg in ws:
+            if not isinstance(msg, WSMessage):
+                continue
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                parsed = json.loads(msg.data)
+                print(parsed)
+                _type = parsed["type"]
+                f = ws_handlers_mapping.get(_type, None)
+                if f is None:
+                    logger.warning("Unknown message type received from client: {}".format(parsed))
+                    continue
+                await f(W, gs, si, parsed)
+            elif msg.type == aiohttp.WSMsgType.ERROR:
+                print('ws connection closed with exception %s' % ws.exception())
+    finally:
+        # When a connection is stopped, remove the connection
+        await remove_player(W, si)
+        print("closing connection for id={}".format(pid))
     return ws
-  # Add user to the client list
-  si = { "id": pid, "socket": ws }
-  W["clients"][pid] = ws
-  try:
-    async for msg in ws:
-      if not isinstance(msg, WSMessage):
-        continue
-      if msg.type == aiohttp.WSMsgType.TEXT:
-        parsed = json.loads(msg.data)
-        print(parsed)
-        _type = parsed["type"]
-        f = ws_handlers_mapping.get(_type, None)
-        if f is None:
-          logger.warning("Unknown message type received from client: {}".format(parsed))
-          continue
-        await f(W, gs, si, parsed)
-      elif msg.type == aiohttp.WSMsgType.ERROR:
-        print('ws connection closed with exception %s' % ws.exception())
-  finally:
-    # When a connection is stopped, remove the connection
-    await remove_player(W, si)
-    print("closing connection for id={}".format(pid))
-  return ws
+
+
+def init_server():
+    words_server_state.ww = load_dictionary(WORDS_DICT_PATH, PARTICLES_PATH)
+
 
 app = web.Application()
 app.add_routes(routes)
 
-words = {}
-particles = {}
-
-def load_dictionary():
-  logger.info("Loading dictionary...")
-  words_dict_path = "./russian-words/words.txt"
-  particles_path = "./russian-words/words.txt"
-  with open(words_dict_path, "r", encoding="windows-1251") as f:
-    for word in f.readlines():
-      words[word[:-2]] = True
-  with open(particles_path, "r", encoding="utf-8") as f:
-    particles = json.load(f)
-  logger.info("Done.")
-  print(words)
-  print(particles)
-
 if __name__ == '__main__':
-  load_dictionary()
-  web.run_app(app, host="127.0.0.1")
+    configure_loggers()
+    logger.info("Running in {} mode".format("DEV" if DEV else "PROD"))
+    init_server()
+    web.run_app(app, host="127.0.0.1")
