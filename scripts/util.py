faa70bd9e307945847ade0e6ff1b50e782391c52
Dmitry Guzeev <dmitri.guzeev@gmail.com>
2021-08-23 19:06:49 +0300

    Fix a big with allowed letters filter in word gen scripts 

--- scripts/util.py
+++ scripts/util.py
@@ -1,5 +1,5 @@
 ALLOWED_CHARS = set([])
-for i in range(ord("а"), ord("я")):
+for i in range(ord("а"), ord("я") + 1):
     ALLOWED_CHARS.add(chr(i))
-for i in range(ord("А"), ord("Я")):
+for i in range(ord("А"), ord("Я") + 1):
     ALLOWED_CHARS.add(chr(i))
